b3b3ff5af7334fdced20de6e1d7aef773d76c04f
zacharybaca <zachary.baca@msn.com>
2023-06-28 18:08:30 -0500

    completed problem 9

--- problems/09-count-scores.js
+++ problems/09-count-scores.js
@@ -29,7 +29,42 @@ console.log(countScores(peeps)); //=> { Anthony: 4, Fred: 4, Winnie: 6 }
 
 function countScores(people) {
   // Your code here
+  //Initialize empty object to store the player and their total score
+  let scoreObj = {};
+  for (let i = 0; i < people.length; i++) {
+    //While iterating through the array, assign the player name and player score to their
+    //own seperate variable
+    let player = people[i].name;
+    let score = people[i].score;
+   //If the key exists in scoreObj, then add that player's score to the existing score
+   //variable on the object
+   //If the key doesn't exist, then add the player as a key, and set their value to the score value
+   //in the array
+    if (scoreObj[player]) {
+      scoreObj[player] += score;
+    } else {
+      scoreObj[player] = score;
+    }
+  }
+  return scoreObj;
 }
 
+// let ppl = [{name: "Anthony", score: 10},
+//             {name: "Fred", score : 10},
+//             {name: "Anthony", score: -8},
+//             {name: "Winnie", score: 12}];
+
+// console.log(countScores(ppl)); //=> { Anthony: 2, Fred: 10, Winnie: 12 }
+
+// let peeps = [
+//   {name: "Anthony", score: 2},
+//   {name: "Winnie", score: 2},
+//   {name: "Fred", score: 2},
+//   {name: "Winnie", score: 2},
+//   {name: "Fred", score: 2},
+//   {name: "Anthony", score: 2},
+//   {name: "Winnie", score: 2}
+// ];
+// console.log(countScores(peeps)); //=> { Anthony: 4, Fred: 4, Winnie: 6 }
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = countScores;
