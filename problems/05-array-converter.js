aef19aa778249276c834ee555b6ccf5b414a46ef
zacharybaca <zachary.baca@msn.com>
2023-06-28 16:20:48 -0500

    completed problem 5

--- problems/05-array-converter.js
+++ problems/05-array-converter.js
@@ -13,7 +13,25 @@ console.log(arrayConverter(["apple", "banana", "potato", "banana"])); // => {app
 
 function arrayConverter(array) {
   // Your code here
+  let fruitObj = {};
+  let count = 1;
+  for (let i = 0; i < array.length; i++) {
+    //Store each element in a variable
+    let key = array[i];
+    //Check to see if key is already in object
+    //If it is, increment its value by 1
+    if (fruitObj[key]) {
+      fruitObj[key] = count += 1
+    } else {
+      //If its not in the object, set the key equal to default count value, which is 1
+      fruitObj[key] = count
+    }
+  }
+  return fruitObj;
 }
 
+// console.log(arrayConverter(["apple", "apple"])); // => {apple: 2}
+// console.log(arrayConverter(["mango", "pineapple"])); // => {mango: 1, pineapple: 1}
+// console.log(arrayConverter(["apple", "banana", "potato", "banana"])); // => {apple: 1, banana: 2, potato: 1}
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = arrayConverter;
